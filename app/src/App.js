1135670071b044e11875245fd3a3de336891db39
jYAMall <e.martsinenko@gmail.com>
2022-06-26 23:13:31 +0300

    App.css

--- app/src/App.js
+++ app/src/App.js
@@ -2,7 +2,7 @@ import React  from 'react';
 import './App.css';
 import Header from './components/Header';
 import Services from './components/Services';
-import Menu from './components/Menu';
+import {Menu} from './components/Menu';
 import Works from './components/Works';
 import Contacts from './components/Contacts';
 
--- app/src/components/Menu.jsx
+++ app/src/components/Menu.jsx
@@ -1,31 +1,41 @@
-import React from 'react'
+
+import React, {useState} from 'react'
 import logo from './logo.svg';
 
-function Menu() {
-  return (
-    
-    <div className='flex blue-glassmorphism menu justify-between text-justify text-black'>
+export const Menu = () => {
+
+    const [state, setState] = useState ('home');
+
+    const onUpdateState = (value) => {
+        setState(value);
+      }
         
-            <img src={logo} alt="logo" className='svgimg flex'/>
 
-            <ul class="nav justify-content-center w-full flex md:justify-center justify-between items-center p-4 ">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#home">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#services">Services</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#works">Works</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#contacts">Contacts</a>
-                </li>
-            </ul>
+    return (
+        
+        <div className='flex  menu justify-between text-justify'>
             
+                <img src={logo} alt="logo" className='svgimg flex'/>
 
-    </div>
-  )
-}
+                <ul className="nav justify-content-center w-full flex md:justify-center justify-between items-center p-4 ">
+                    <li className="nav-item">
+                        <a className={state === 'home' ? 'active nav-link' : 'nav-link'} onClick={() => onUpdateState('home')} href="#home">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className={state === 'services' ? 'active nav-link' : 'nav-link'} onClick={() => onUpdateState('services')} href="#services">Services</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className={state === 'works' ? 'active nav-link' : 'nav-link'} onClick={() => onUpdateState('works')} href="#works">Works</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className={state === 'contacts' ? 'active nav-link' : 'nav-link'} onClick={() => onUpdateState('contacts')} href="#contacts">Contacts</a>
+                    </li>
+                </ul>
 
-export default Menu
+                <button className='bt'>Сontact now</button>
+                
+
+        </div>
+    )
+    }
+
