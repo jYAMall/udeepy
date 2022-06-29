21de3852ed014751d3f014ff47d7a31520188462
jYAMall <e.martsinenko@gmail.com>
2022-06-29 19:09:53 +0300

    App.css

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -4,7 +4,23 @@ function Contacts() {
   return (
     <section id='contacts'>
         <div className='box bg4'>
-            Contacts
+          <form>
+                    <div>
+                        <label>Name</label>
+                        <input type="text" placeholder='Enter your name' />
+                    </div>
+                    <div>
+                        <label>Email</label>
+                        <input type="email" placeholder='Enter your email' />
+                    </div>
+                    <div>
+                        <label>Message</label>
+                        <textarea cols="30" rows="15" placeholder='Enter your message'></textarea>
+                    </div>
+                    <div className="bottom">
+                        <button className="btn btn-dark">Submit</button>
+                    </div>
+            </form>
         </div>
     </section>
   )
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,6 +2,9 @@
 import React, {useState} from 'react'
 import logo from './logo.svg';
 import { BsGithub, BsLinkedin, BsFacebook } from 'react-icons/bs';
+import { AiOutlineClose} from 'react-icons/ai';
+import { HiMenuAlt4} from 'react-icons/hi';
+
 
 export const Menu = () => {
 
@@ -10,7 +13,16 @@ export const Menu = () => {
     const onUpdateState = (value) => {
         setState(value);
       }
-        
+    
+    const [toggleMenu, setToggleMenu] =useState(false);
+
+    const NavBarItem = ({ title, classProps }) => {
+        return (
+            <li className={`mx-4 cursor-pointer ${classProps}`}>
+                {title}
+            </li>
+        );
+    }
 
     return (
         
@@ -18,7 +30,7 @@ export const Menu = () => {
             
                 <img src={logo} alt="logo" className='svgimg flex '/>
 
-                <ul className="nav justify-content-center w-full flex md:justify-center justify-between items-center p-4 ">
+                <ul className="nav  justify-content-center w-full md:justify-center md:flex hidden justify-between items-center p-4 ">
                     <li className="nav-item">
                         <a className={state === 'home' ? 'active nav-link' : 'nav-link'} onClick={() => onUpdateState('home')} href="#home">Home</a>
                     </li>
@@ -33,9 +45,51 @@ export const Menu = () => {
                     </li>
                 </ul>
 
-                <nav className='button-social' href='#home'><BsGithub/></nav>
-                <nav className='button-social' href='#home'><BsLinkedin/></nav>
-                <nav className='button-social' href='#home'><BsFacebook/></nav>
+                <a className='button-social md:flex hidden' href='#home'><BsGithub/></a>
+                <a className='button-social md:flex hidden' href='#home'><BsLinkedin/></a>
+                <a className='button-social md:flex hidden' href='#home'><BsFacebook/></a>
+
+                <div className='flex relative'>
+                {toggleMenu
+                ? <AiOutlineClose frontSize={28} className='text-white md:hidden cursor-pointer' onClick={() => setToggleMenu (false)} />
+                : <HiMenuAlt4 frontSize={28} className='text-white md:hidden cursor-pointer' onClick={() => setToggleMenu (true)}/>
+                }
+                {toggleMenu && (
+                    <ul
+                        className='z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+                            flex flex-col justify-start items-center rounded-md blue-glassmorphism text-white animate-slide-in'
+                    >
+                        <li className='text-xl w-full my-2'>
+                            <AiOutlineClose onClick={() => setToggleMenu(false)} />
+                        </li>
+                        {[
+                            
+                            <ul className="text-center">
+                                <li className="nav-item py-8 ">
+                                    <a className={state === 'home' ? 'active nav-link' : 'nav-link'} onClick={() => onUpdateState('home')} href="#home"><p>Home</p></a>
+                                </li>
+                                <li className="nav-item py-8">
+                                    <a className={state === 'services' ? 'active nav-link' : 'nav-link'} onClick={() => onUpdateState('services')} href="#services"><p>Services</p></a>
+                                </li>
+                                <li className="nav-item py-8">
+                                    <a className={state === 'works' ? 'active nav-link' : 'nav-link'} onClick={() => onUpdateState('works')} href="#works"><p>Works</p></a>
+                                </li>
+                                <li className="nav-item py-8">
+                                    <a className={state === 'contacts' ? 'active nav-link' : 'nav-link'} onClick={() => onUpdateState('contacts')} href="#contacts"><p>Contacts</p></a>
+                                </li>
+                            </ul>
+
+                        ].map((item, index) => (
+                            <NavBarItem key={item + index} title={item} classProps='my-2 text-lg'/>
+                        ))} 
+                            <div className='flex '>
+                                <nav className='button-social-m mx-8' href='#home'><BsGithub/></nav>
+                                <nav className='button-social-m mx-8' href='#home'><BsLinkedin/></nav>
+                                <nav className='button-social-m mx-8' href='#home'><BsFacebook/></nav> 
+                            </div>
+                    </ul>
+                )}
+            </div>
                 
 
         </div>
